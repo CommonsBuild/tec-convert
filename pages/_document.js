0c8776d29f03abb849afe520d96514a0f7ab0d8e
David Llop <931684+sembrestels@users.noreply.github.com>
2022-01-25 05:02:31 -0500

    Change twitter handle

--- pages/_document.js
+++ pages/_document.js
@@ -50,19 +50,19 @@ export default class Document extends NextDocument {
           <meta name="theme-color" content="#000000" />
 
           <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@AragonProject" />
+          <meta name="twitter:site" content="@tecmns" />
           <meta name="twitter:description" content={env('SITE_DESCRIPTION')} />
           <meta name="twitter:title" content={env('SITE_TITLE')} />
-          <meta name="twitter:image" content={env('SITE_URL')+"/twitter-card-icon.png"} />
+          <meta name="twitter:image" content={env('SITE_URL')+"twitter-card-icon.png"} />
 
           <meta property="og:title" content={env('SITE_TITLE')} />
           <meta property="og:description" content={env('SITE_DESCRIPTION')} />
           <meta property="og:url" content={env('SITE_URL')} />
           <meta property="og:site_name" content={env('SITE_TITLE')} />
-          <meta property="og:image" content={env('SITE_URL')+"/twitter-card-icon.png"} />
+          <meta property="og:image" content={env('SITE_URL')+"twitter-card-icon.png"} />
           <meta
             property="og:image:secure_url"
-            content={env('SITE_URL')+"/twitter-card-icon.png"}
+            content={env('SITE_URL')+"twitter-card-icon.png"}
           />
           <meta property="og:image:width" content="300" />
           <meta property="og:image:height" content="300" />
